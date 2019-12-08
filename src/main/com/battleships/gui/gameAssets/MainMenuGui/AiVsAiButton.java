0a3e0fb95f5205d07da9b57d2472511694068c72
EleGanTor <sascha@moessle.com>
2019-12-08 18:03:44 +0100

    optimized Button classes

---
+++ src/main/com/battleships/gui/gameAssets/MainMenuGui/AiVsAiButton.java
@@ -0,0 +1,21 @@
+package com.battleships.gui.gameAssets.MainMenuGui;
+
+import com.battleships.gui.fontRendering.TextMaster;
+import com.battleships.gui.guis.GuiManager;
+import com.battleships.gui.main.Inits;
+import com.battleships.gui.renderingEngine.Loader;
+
+public class AiVsAiButton extends MainMenuButton {
+    public AiVsAiButton(GuiManager guiManager, Loader loader) {
+        super(guiManager, loader);
+    }
+
+    @Override
+    protected void clickAction() {
+        TextMaster.clear();
+        //Will be changed in final version
+        Inits.setGlobalGameState(1);
+        //MenuTest.GlobalGameState = 1;
+        //TODO start a Ai vs Ai game
+    }
+}
---
+++ src/main/com/battleships/gui/gameAssets/MainMenuGui/PVsAiButton.java
@@ -0,0 +1,21 @@
+package com.battleships.gui.gameAssets.MainMenuGui;
+
+import com.battleships.gui.fontRendering.TextMaster;
+import com.battleships.gui.guis.GuiManager;
+import com.battleships.gui.main.Inits;
+import com.battleships.gui.renderingEngine.Loader;
+
+public class PVsAiButton extends MainMenuButton {
+    public PVsAiButton(GuiManager guiManager, Loader loader) {
+        super(guiManager, loader);
+    }
+
+    @Override
+    protected void clickAction() {
+        TextMaster.clear();
+        //Will be changed in final version
+        Inits.setGlobalGameState(1);
+        //MenuTest.GlobalGameState = 1;
+        //TODO start a player vs ai game
+    }
+}
